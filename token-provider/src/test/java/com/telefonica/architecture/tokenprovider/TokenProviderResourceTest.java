5a3f89cde1c8f9c33e162c6ab46f4438671f084a
Di Grazia <NDIGRAZIA@tmoviles.com.ar>
2020-07-13 14:23:17 -0300

    Update Test cases

--- token-provider/src/test/java/com/telefonica/architecture/tokenprovider/TokenProviderResourceTest.java
+++ token-provider/src/test/java/com/telefonica/architecture/tokenprovider/TokenProviderResourceTest.java
@@ -4,7 +4,6 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 public class TokenProviderResourceTest {
@@ -12,10 +11,9 @@ public class TokenProviderResourceTest {
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/hello")
+          .when().get("/token")
           .then()
-             .statusCode(200)
-             .body(is("hello"));
+             .statusCode(200);
     }
 
 }
